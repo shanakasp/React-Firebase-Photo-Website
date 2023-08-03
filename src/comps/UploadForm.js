9c2ad34112e52e0f32fdc387171eec1db53cfa90
shanakasp <74164029+shanakasp@users.noreply.github.com>
2023-08-03 18:05:07 +0530

    modified UploadForm.js in comps

--- src/comps/UploadForm.js
+++ src/comps/UploadForm.js
@@ -1,15 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 function UploadForm() {
+  const [file, setFile] = useState(null);
+  const [error, setError] = useState(null);
+
+  const types = ["image/png", "image/jpeg"];
+
   const changeHandler = (e) => {
     let selected = e.target.files[0];
     console.log(selected);
+
+    if (selected && types.includes(selected.type)) {
+      setFile(selected);
+    } else {
+      setFile(null);
+      setError("Please choose correct file type.");
+    }
   };
 
   return (
     <div>
       <form>
-        <input type="file" onchange={changeHandler} />
+        <input type="file" onChange={changeHandler} />
+        <div className="output">
+          {error && <div className="error"> {error} </div>}
+        </div>
       </form>
     </div>
   );
